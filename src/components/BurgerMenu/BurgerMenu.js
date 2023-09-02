37f2f7fe69f1122f426c98011bf4522a8049faca
Timofey Voronov <timmycarousel@gmail.com>
2023-09-02 22:08:37 +0300

    оверлей затемнение и клик-закрытие

--- src/components/BurgerMenu/BurgerMenu.js
+++ src/components/BurgerMenu/BurgerMenu.js
@@ -6,8 +6,13 @@ import Burger from "../../images/burger.svg";
 
 export default function BurgerMenu() {
   const [isOpen, setIsOpen] = useState(false);
+
   const toggleMenu = () => {
     setIsOpen(!isOpen);
+  };
+
+  const closeMenu = () => {
+    setIsOpen(false);
   };
 
   return (
@@ -16,10 +21,12 @@ export default function BurgerMenu() {
         <img src={Burger} alt="бургер меню" />
       </button>
 
+      {/* Оверлей для затемнения оставшегося контента */}
       <div
         className={`burger-menu__overlay ${isOpen ? "open" : ""}`}
-        onClick={toggleMenu}
+        onClick={closeMenu}
       />
+
       <nav className={`burger-menu__container ${isOpen ? "open" : ""}`}>
         <img
           onClick={toggleMenu}
